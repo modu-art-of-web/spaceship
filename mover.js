1d97d7deef692af715e0a475723d7b9850a8e151
mojosoeun <mojosoeun@gmail.com>
2017-02-19 00:17:19 +0900

    feat: add some gravity attraction according to frequency avg

---
+++ mover.js
@@ -0,0 +1,51 @@
+var Mover = function(m, x, y) {
+  this.mass = m;
+  this.position = createVector(x, y);
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
+
+  this.applyForce = function(force) {
+    var f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  };
+
+  this.update = function() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  };
+
+  this.display = function() {
+    stroke(0);
+    strokeWeight(2);
+    fill(255, 127);
+    ellipse(this.position.x, this.position.y, this.mass*16, this.mass*16);
+  };
+
+  this.calculateAttraction = function(m) {
+    var force = p5.Vector.sub(this.position, m.position);
+    var distance = force.mag();
+    distance = constrain(distance, 5.0, 25.0);
+    force.normalize();
+    var strength = (G * this.mass * m.mass) / (distance * distance);
+    force.mult(strength);
+    return force;
+  };
+
+  this.checkEdges = function() {
+    if (this.position.x > width) {
+      this.position.x = width;
+      this.velocity.x *= -1;
+    } else if (this.position.x < 0) {
+      this.velocity.x *= -1;
+      this.position.x = 0;
+    }
+    if (this.position.y > height) {
+      this.velocity.y *= -1;
+      this.position.y = height;
+    } else if (this.position.y < 0) {
+      this.velocity.y *= -1;
+      this.position.y = 0;
+    }
+  };
+};
---
+++ sketch.js
@@ -0,0 +1,71 @@
+var movers = [];
+
+var G = 1;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  for (var i = 0; i < 100; i++) {
+    movers[i] = new Mover(random(1, 1), random(width), random(height));
+  }
+}
+
+function draw() {
+  background(51);
+  analyser.getByteFrequencyData(dataArray);
+  var sum = 0;
+  for (var i=0, j = dataArray.length; i < j; i ++){
+    sum += dataArray[i];
+  }
+
+  var avg = sum / 128;
+
+  G = avg / 15;
+
+  for (var i = 0; i < movers.length; i++) {
+    for (var j = 0; j < movers.length; j++) {
+      if (i !== j) {
+        var force = movers[j].calculateAttraction(movers[i]);
+        movers[i].applyForce(force);
+      }
+    }
+
+    movers[i].update();
+    movers[i].checkEdges();
+    movers[i].display();
+
+  }
+}
+
+function play(track){
+  SC.resolve(track)
+    .then(function(data){
+      var streamUrl = data.stream_url + '?client_id=' + client_id;
+      console.log(streamUrl);
+      audio.setAttribute('src', streamUrl);
+      audio.play()
+    }).catch(function(error){
+      console.log(error);
+    });
+}
+
+
+var client_id = '802c2f1c80c96881ff265799929e8a2c';
+
+SC.initialize({
+  client_id: client_id
+});
+
+var audio = document.querySelectorAll('.ctrgroup__player__audio')[0];
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)
+  , source = audioCtx.createMediaElementSource(audio)
+  , analyser = audioCtx.createAnalyser();
+
+  analyser.fftSize = 256;
+  analyser.connect(audioCtx.destination);
+  source.connect(analyser);
+  audio.crossOrigin = "anonymous";
+
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+play('https://soundcloud.com/louisthechild/brokenrecord');
