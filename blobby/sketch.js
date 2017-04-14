cc7eeb490abd5015483a55c170ea2681e97182b3
mojosoeun <mojosoeun@gmail.com>
2017-04-14 09:50:48 +0900

    feat: add butterfly effect

--- blobby/sketch.js
+++ blobby/sketch.js
@@ -1,16 +1,82 @@
 var ball;
 var width = window.innerWidth
 , height = window.innerHeight;
+var avg = 0;
+
+var yoff = 0;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+
   ball = new Ball(width / 2, height / 2, width / 5);
 }
 
 function draw() {
-  background('#468966');
 
-  ball.show();
+  analyser.getByteFrequencyData(dataArray);
+  var sum = 0;
+  for (var i=0, j = dataArray.length; i < j; i ++){
+    sum += dataArray[i];
+  }
+
+  avg = sum / 8092;
+
+  // background(51);
+  background('#468966');
+  translate(width / 2, height / 2);
+
+  rotate(PI / 2);
+  beginShape();
+  noStroke();
+  // stroke(255);
+  // fill(255, 50);
+  fill('#FFF0A5');
+  strokeWeight(1);
+
+  var da = PI / 100;
+  var dx = 0.1
+
+  var xoff = 0;
+  for (var a = - PI/2; a <= PI/2;  a+= da ) {
+    var n = noise(xoff, yoff);
+    var r = sin(2 * a) * map(n, 0, 1, 50, 100); // pollar rose
+    // var x = sin(frameCount * 0.01) * r * cos(a);
+    var y = r * sin(a) * avg;
+    var x = r * cos(a) * avg;
+    // var y = sin(yoff) * r * sin(a);
+    xoff += dx;
+
+    // point(x, y);
+    vertex(x, y);
+    // r--;
+  }
+  // endShape();
+
+  // beginShape();
+  // var xoff = 0;
+  for (var a = PI/2; a <= 3 * PI / 2;  a+= da ) {
+    var n = noise(xoff, yoff);
+    var r = sin(2 * a) * map(n, 0, 1, 50, 100);
+    // var x = r * cos(a);
+    // var x = sin(frameCount * 0.01) * r * cos(a);
+    var y = r * sin(a) * avg;
+
+    var x = r * cos(a) * avg;
+    // var y = sin(yoff) * r * sin(a);
+    xoff -= dx;
+
+    // point(x, y);
+    vertex(x, y);
+    // r--;
+  }
+  endShape();
+
+  yoff += 0.1;
+
+
+
+
+  // ball.show();
 }
 
 function Ball(x, y, r) {
@@ -20,13 +86,7 @@ function Ball(x, y, r) {
 
   this.show = function() {
 
-    analyser.getByteFrequencyData(dataArray);
-    var sum = 0;
-    for (var i=0, j = dataArray.length; i < j; i ++){
-      sum += dataArray[i];
-    }
 
-    var avg = sum / 64;
     push();
     translate(this.pos.x, this.pos.y);
     noStroke();
